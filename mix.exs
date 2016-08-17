6c356d2d21dbda438ba907861b8f115596800162
Yuya Yabe <yuyabee@gmail.com>
2016-08-17 06:56:33 +0000

    Bump v0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudVision.Mixfile do
 
   def project do
     [app: :cloud_vision,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
