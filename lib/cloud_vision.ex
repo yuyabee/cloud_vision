3c38d70826b63bd33fa2177ac06c6916512f93c5
Yuya Yabe <yuyabee@gmail.com>
2016-08-17 06:55:57 +0000

    Implement analyze/2 to accept local file

--- lib/cloud_vision.ex
+++ lib/cloud_vision.ex
@@ -1,21 +1,33 @@
 defmodule CloudVision do
-  def analyze(img_path) do
-    params = %{
-      requests: [%{
-        image: %{
-          source: %{
-            gcsImageUri: "gs://" <> Application.get_env(:cloud_vision, :gcsUri) <> "/" <> img_path
-          }
-        },
-        features: [
-          %{
-            type: "LABEL_DETECTION"
-          }, %{
-            type: "LOGO_DETECTION"
-          }, %{
-            type: "TEXT_DETECTION"
-          }
-        ]}]}
+  def analyze(img_path) when is_bitstring(img_path) do
+    analyze(img_path, :local)
+  end
+
+  def analyze(img_path, :local) do
+    %{content: Base.encode64(File.read!(img_path))}
+    |> analyze
+  end
+
+  def analyze(img_path, :storage) do
+    %{source: %{
+      gcsImageUri: "gs://" <> Application.get_env(:cloud_vision, :gcsUri) <> "/" <> img_path
+    }}
+    |> analyze
+  end
+
+  def analyze(img) do
+    params =
+      %{requests: [%{
+         image: img,
+         features: [
+           %{
+             type: "LABEL_DETECTION"
+           }, %{
+             type: "LOGO_DETECTION"
+           }, %{
+             type: "TEXT_DETECTION"
+           }
+         ]}]}
 
     case CloudVision.Client.post("/images:annotate", params |> Poison.encode!) do
       {:ok, %HTTPoison.Response{body: res}} -> Poison.decode!(res)
